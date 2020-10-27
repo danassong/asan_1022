b7554573082ca61009ad58a9b852f111292bc59d
DanaSong <dahae0624@naver.com>
2020-10-27 16:16:31 +0900

    10

--- js/asan.js
+++ js/asan.js
@@ -21,61 +21,74 @@
   // });
 
 
+  var usedata 
+  $.ajax({
+    type: 'GET',
+    url: 'data/doctors.json',
+    // beforesend: 혹시나 json파일이 아니라면 바꿔주는것
+    beforeSend: function(xhr) {
+      if (xhr.overrideMimeType) {
+        xhr.overrideMimeType('application/json')
+      }
+    },
+    success: function(data) {
+      usedata = data
+    },
+    error: function() {
+      alert(xhr.status + '오류발생')
+    }
+  })
 
-  // `<li><div class="img"><img src="${photo}" alt=""></div>`
-  // `<div class="doctorInfo"><strong>${name}</strong>`
-  // `<p>${depart}</p>`
-  // `<div>${about}</div></div></li>`
   $('#container').on('click', '.medicalContent .mediList a', function(e){
     e.preventDefault()
     var url = this.href;
     var part = this.id
     $("#container > #content").remove();
-    $("#container").load(url + " #content");
-    
-    $.ajax({
-      type: 'GET',
-      url: 'data/doctors.json',
-      // beforesend: 혹시나 json파일이 아니라면 바꿔주는것
-      beforeSend: function(xhr) {
-        if (xhr.overrideMimeType) {
-          xhr.overrideMimeType('application/json')
-        }
-      },
-      success: function(data) {
-        var usedata = data[part]
-        var newContent = ''
-        function dataPrint() {
-          for (var i in usedata) {
-            newContent += `<li><div class="img"><img src="${usedata[i].photo}" alt=""></div>`
-            newContent += `<div class="doctorInfo"><strong>${usedata[i].name}</strong>`
-            newContent += `<p>${usedata[i].depart}</p>`
-            newContent += `<div>${usedata[i].about}</div></div></li>`
-          }
-          $('#content .part1DoctorList').html(`<ul>${newContent}</ul>`)
-        }
-        dataPrint()
-
-      },
-      error: function() {
-        alert(xhr.staus + '오류발생')
+    $("#container").load(url + " #content", function() {
+      var newContent = ''
+      for (var i in usedata[part]) {
+        newContent += `<li><div class="img"><img src="${usedata[part][i].photo}" alt=""></div>`
+        newContent += `<div class="doctorInfo"><strong>${usedata[part][i].name}</strong>`
+        newContent += `<p>${usedata[part][i].depart}</p>`
+        newContent += `<div>${usedata[part][i].about}</div></div></li>`
       }
+      $('#content .part1DoctorList').html(`<ul>${newContent}</ul>`)
+    });
+
+
+  })
+
+
+  /* 햄버거 버튼 클릭시 네비박스 열기 */
+  $('#lnb_menu').on('click', function() {
+    $(this).next().css({
+      display: 'block'
     })
+    $('#lnb').animate({
+      left: '0px'
+    }, 500)
+  })
 
-    return false;
-
+  $('#lnb_close').on('click', function() {
+    $('#lnb').animate({
+      left: '-274px'
+    }, 500, function() {
+      $('#navWrap').css({
+        display: 'none'
+      })
+    })
   })
 
 
   /* 스크롤이벤트 */
-  $(window).scroll(function() {
-    var sct = $(this).scrollTop()
-    if (sct >= 50 && !$('#header').hasClass('on')) {
-      $('#header').slideUp(100).slideDown(100).addClass('on')
-    } else if ( sct < 50 && $('#header').hasClass('on')) {
-      $('#header').removeClass('on')
-    }
-  })
+  // $(window).scroll(function() {
+  //   var sct = $(this).scrollTop()
+  //   if (sct >= 50 && !$('#header').hasClass('on')) {
+  //     $('#header').slideUp(100).slideDown(100).addClass('on')
+  //   } else if ( sct < 50 && $('#header').hasClass('on')) {
+  //     $('#header').removeClass('on')
+  //   }
+  // })
 
 
 
